47013e9532cf1069d78bc0c968531dc135a2a1b8
vasconcelos <di0g0vasconcelos@hotmail.com>
2022-05-29 22:18:30 -0300

    inicio do repositório

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TarefaController extends Controller
+{
+    //ao invés de implamentamos na rota o middleware podemos fazê-lo pela o controlador
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    
+    // }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        //para verificar se o usuario estar logado, quando as rotas ou controlador não estar protegido, não há necessidade
+        // if(Auth::check()){
+        //     $id = Auth::user()->id;
+        //     $name = Auth::user()->name;
+        //     $email = Auth::user()->email;
+        //     echo "Voce está logado no sistema $id | $name | $email";
+        // }else {
+        //     echo "Voce não está logado no sistema";
+        // }
+        //outra forma 
+        // if(auth()->check()){
+        //     $id = auth()->user()->id;
+        //     $name = auth()->user()->name;
+        //     $email = auth()->user()->email;
+        //     echo "Voce está logado no sistema $id | $name | $email";
+        // }else {
+        //     echo "Voce não está logado no sistema";
+        // }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        //
+    }
+}
